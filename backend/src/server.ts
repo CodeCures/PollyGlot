68ad73e85c6415f966f973732ce75ac50ad917d9
CodeCures <couragesblog@gmail.com>
2025-01-21 15:02:53 +0100

    add api route middleware

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { config } from 'dotenv';
+import langTranslateRoute from './routes/index'
 
 config()
 
@@ -8,7 +9,7 @@ const app = express()
     .use(cors())
     .use(express.json())
 
-
+app.use('/api', langTranslateRoute)
 
 app.listen(
     process.env.PORT,
