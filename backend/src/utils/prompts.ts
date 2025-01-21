fe9404b3a41e368c1f53c23711342d2531422945
CodeCures <couragesblog@gmail.com>
2025-01-21 15:04:25 +0100

    configure openai system prompt

---
+++ backend/src/utils/prompts.ts
@@ -0,0 +1,11 @@
+import { ChatCompletionMessageParam } from "openai/resources";
+
+export const messages = (text: string, language: string): ChatCompletionMessageParam[] => {
+    return [
+        { role: "system", content: "you are a polyglot language translator, you have excellent understand of French, Spanish and Japanese language. you will accept input from users in english and translate to their selected language. You are expected to be formal in your translation. You are to return only the translated text and don't add any other things" },
+        {
+            role: "user",
+            content: `input: ${text}; language: ${language}`,
+        },
+    ]
+}
