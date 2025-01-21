1782becf9678441c1f65006f9cda67838db87546
CodeCures <couragesblog@gmail.com>
2025-01-21 13:42:22 +0100

    Create an instance of cors server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,16 @@
-console.log('this is the server');
+import express from 'express';
+import cors from 'cors';
+import { config } from 'dotenv';
+
+config()
+
+const app = express()
+    .use(cors())
+    .use(express.json())
+
+
+
+app.listen(
+    process.env.PORT,
+    () => console.log(`server running on port ${process.env.PORT}`)
+)
