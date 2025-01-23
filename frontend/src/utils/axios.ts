57e4d3c660b98364c9a5856109d14329cd7431cb
CodeCures <couragesblog@gmail.com>
2025-01-23 21:42:08 +0100

    Set axios base url

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: 'https://pollyglot-backend.azurewebsites.net/api',
     headers: {
         "Content-Type": "application/json"
     }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      // Remove the `external` option or only use it if you have a specific reason.
-    }
-  },
-  define: {
-    'process.env': process.env
-  }
 })
