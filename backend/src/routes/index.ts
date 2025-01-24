6b394ba33057f5d8d3833f9ef383dfabc6f9830e
CodeCures <couragesblog@gmail.com>
2025-01-24 13:59:49 +0100

    Add image generation route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { translate } from "../controllers/LanguageTranslateController";
+import { generateImage, translate } from "../controllers/LanguageTranslateController";
 
 const router = Router()
 
 router.post('/translate', translate)
+router.post('/generate-image', generateImage)
 
 export default router;
